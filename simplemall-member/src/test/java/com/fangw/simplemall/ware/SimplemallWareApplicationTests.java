806849adb91dad9d6e8b7dac95e565a8034b5f79
fangwei <1779219498@qq.com>
2023-08-14 21:48:55 +0800

    项目初步搭建

---
+++ simplemall-member/src/test/java/com/fangw/simplemall/ware/SimplemallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fangw.simplemall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplemallWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ simplemall-product/src/main/java/com/fangw/simplemall/order/SimplemallWareApplication.java
@@ -0,0 +1,13 @@
+package com.fangw.simplemall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemallWareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemallWareApplication.class, args);
+	}
+
+}
