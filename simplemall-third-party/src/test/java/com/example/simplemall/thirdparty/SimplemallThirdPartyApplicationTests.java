85fdf4d2a575dd1d0f09059bf65c2753b313a7e6
fangwei <1779219498@qq.com>
2023-08-21 22:25:36 +0800

    Description: oss上传前后端联调成功

---
+++ simplemall-third-party/src/test/java/com/example/simplemall/thirdparty/SimplemallThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplemall.thirdparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplemallThirdPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
