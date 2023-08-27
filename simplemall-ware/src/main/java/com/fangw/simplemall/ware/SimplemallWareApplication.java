0590949d4f3747c80f049bbc54effb3c4216ccf5
fangwei <1779219498@qq.com>
2023-08-27 21:29:25 +0800

    Description: 仓储服务 仓库管理查询库存

--- simplemall-ware/src/main/java/com/fangw/simplemall/ware/SimplemallWareApplication.java
+++ simplemall-ware/src/main/java/com/fangw/simplemall/ware/SimplemallWareApplication.java
@@ -3,13 +3,15 @@ package com.fangw.simplemall.ware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableTransactionManagement
 public class SimplemallWareApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimplemallWareApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimplemallWareApplication.class, args);
+    }
 
 }
