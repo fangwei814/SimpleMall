888e37916e5585f3a53d8cb64c4ec1513d2f3738
fangwei <1779219498@qq.com>
2023-08-31 23:32:40 +0800

    Description: 搜索服务 修改了搜索页面和主页的部分代码，目前可以成功跳转

---
+++ simplemall-search/src/main/java/com/fangw/simplemall/search/controller/SearchController.java
@@ -0,0 +1,12 @@
+package com.fangw.simplemall.search.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SearchController {
+    @GetMapping("/list.html")
+    public String listPage() {
+        return "list";
+    }
+}
