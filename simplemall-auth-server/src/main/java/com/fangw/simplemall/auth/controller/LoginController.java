a07bbc5f97cd29cea2f32075d19e49d3967566c2
fangwei <1779219498@qq.com>
2023-09-03 18:34:21 +0800

    Description: 验证服务 短信登录

--- simplemall-auth-server/src/main/java/com/fangw/simplemall/auth/controller/LoginController.java
+++ simplemall-auth-server/src/main/java/com/fangw/simplemall/auth/controller/LoginController.java
@@ -1,13 +1,18 @@
 package com.fangw.simplemall.auth.controller;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fangw.common.constant.AuthServerConstant;
+import com.fangw.common.exception.BizCodeEnum;
 import com.fangw.common.utils.R;
 import com.fangw.simplemall.auth.feign.ThirdPartyFeignService;
 
@@ -15,6 +20,8 @@ import com.fangw.simplemall.auth.feign.ThirdPartyFeignService;
 public class LoginController {
     @Autowired
     private ThirdPartyFeignService thirdPartyFeignService;
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     /**
      * 发送短信
@@ -25,7 +32,20 @@ public class LoginController {
     @ResponseBody
     @GetMapping("/sms/sendcode")
     public R sendCode(@RequestParam("phone") String phone) {
+        // 1.查询redis中的验证码，用redis防止非法大量调用验证码接口
+        String redisCode = redisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + phone);
+        if (StringUtils.isNotBlank(redisCode)) {
+            long codeLong = Long.parseLong(redisCode.split("_")[1]);
+            if (System.currentTimeMillis() - codeLong < 60000) {
+                // 60秒内不能重复发送
+                return R.error(BizCodeEnum.SMS_CODE_EXCEPTION.getCode(), BizCodeEnum.SMS_CODE_EXCEPTION.getMsg());
+            }
+        }
+
+        // 2.缓存到redis
         String code = UUID.randomUUID().toString().substring(0, 5);
+        redisTemplate.opsForValue().set(AuthServerConstant.SMS_CODE_CACHE_PREFIX + phone,
+            code + "_" + System.currentTimeMillis(), 5, TimeUnit.MINUTES);
 
         thirdPartyFeignService.sendCode(phone, code);
         return R.ok();
--- simplemall-third-party/src/main/java/com/fanw/simplemall/thirdparty/controller/SmsSendController.java
+++ simplemall-third-party/src/main/java/com/fanw/simplemall/thirdparty/controller/SmsSendController.java
@@ -1,21 +1,24 @@
 package com.fanw.simplemall.thirdparty.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fangw.common.utils.R;
+import com.fanw.simplemall.thirdparty.component.SmsComponent;
 
 @RestController
 @RequestMapping("/sms")
 public class SmsSendController {
-    // @Autowired
-    // SmsComponent smsComponent;
+    @Autowired
+    SmsComponent smsComponent;
 
-    @GetMapping("/hello")
-    public R hello() {
-        return R.ok().setData("hello");
-    }
+    // @GetMapping("/hello")
+    // public R hello() {
+    // return R.ok().setData("hello");
+    // }
 
     /**
      * 提供给别的服务进行调用
@@ -26,9 +29,9 @@ public class SmsSendController {
      *            验证码
      * @return
      */
-    // @GetMapping("/sendcode")
-    // public R sendCode(@RequestParam("phone") String phone, @RequestParam("code") String code) {
-    // smsComponent.sendSmsCode(phone, code);
-    // return R.ok();
-    // }
+    @GetMapping("/sendcode")
+    public R sendCode(@RequestParam("phone") String phone, @RequestParam("code") String code) {
+        smsComponent.sendSmsCode(phone, code);
+        return R.ok();
+    }
 }
