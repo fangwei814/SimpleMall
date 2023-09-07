9eae89826e7fb7aab6eb3e0cac5ac9c5d8cffb1a
fangwei <1779219498@qq.com>
2023-09-07 18:50:02 +0800

    Description: 秒杀服务 登录检查，利用SpringSession 订单服务 支付模块不做了

--- simplemall-seckill/src/main/java/com/fangw/simplemall/seckill/SimplemallSeckillApplication.java
+++ simplemall-seckill/src/main/java/com/fangw/simplemall/seckill/SimplemallSeckillApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @EnableFeignClients(basePackages = "com.fangw.simplemall.seckill.feign")
 @EnableScheduling
 @EnableRabbit
